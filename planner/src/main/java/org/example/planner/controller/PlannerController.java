222aa4263e165ff98a945af82dc6493aa8379007
rlagywnd4 <rlagywnd4@gmail.com>
2024-12-10 12:32:41 +0900

    fix: delete useless log.info()

--- planner/src/main/java/org/example/planner/controller/PlannerController.java
+++ planner/src/main/java/org/example/planner/controller/PlannerController.java
@@ -1,7 +1,6 @@
 package org.example.planner.controller;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.example.planner.dto.PlannerRequestDTO;
 import org.example.planner.dto.PlannerResponseDTO;
 import org.example.planner.service.PlannerService;
@@ -14,7 +13,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/planner")
 @RequiredArgsConstructor
-@Slf4j
 public class PlannerController {
 
     private final PlannerService plannerService;
@@ -28,17 +26,14 @@ public class PlannerController {
 
     @GetMapping("/{id}")
     public ResponseEntity<PlannerResponseDTO> findPlannerById(@PathVariable Long id){
-        log.info("start find by id");
         PlannerResponseDTO planner = plannerService.findPlannerById(id);
 
-        log.info("hihihihihi");
         log.debug(planner.toString());
         return ResponseEntity.ok(planner);
     }
 
     @GetMapping
     public ResponseEntity<List<PlannerResponseDTO>> findAllPlanner(){
-        log.info("start find all");
         List<PlannerResponseDTO> planner = plannerService.findAllPlanner();
 
         return ResponseEntity.ok(planner);
